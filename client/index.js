cf93a39a0c4d12ea5c1925a3fb3704b2b8986624
Steven Rowney <steven.rowney@reaktor.com>
2019-04-21 15:42:51 +0400

    Only register in development mode
    Signed-off-by: Steven Rowney <steven.rowney@reaktor.com>

--- client/index.js
+++ client/index.js
@@ -4,12 +4,14 @@ import React from "react";
 import { registerOperatorRoute } from "../../../../client/ui";
 import DevTools from "./components/devtools";
 
-registerOperatorRoute({
-  isNavigationLink: true,
-  isSetting: true,
-  path: "/dev-tools",
-  mainComponent: DevTools,
-  // eslint-disable-next-line react/display-name, react/no-multi-comp
-  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLaptop} {...props} />,
-  sidebarI18nLabel: "admin.settings.devToolsLabel"
-});
+if (process.env.NODE_ENV === "development") {
+  registerOperatorRoute({
+    isNavigationLink: true,
+    isSetting: true,
+    path: "/dev-tools",
+    mainComponent: DevTools,
+    // eslint-disable-next-line react/display-name, react/no-multi-comp
+    SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLaptop} {...props} />,
+    sidebarI18nLabel: "admin.settings.devToolsLabel"
+  });
+}
