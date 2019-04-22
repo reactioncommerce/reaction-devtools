7ee2df222b4f003f0aeba17a34512d698f7e02ef
Steven Rowney <steven.rowney@reaktor.com>
2019-04-22 10:57:06 +0400

    Clean code for auto enable

--- register.js
+++ register.js
@@ -1,24 +1,22 @@
 import { Reaction } from "/lib/api";
 
-if (process.env.NODE_ENV === "development") {
-  // Register package as ReactionCommerce package
-  Reaction.registerPackage({
-    label: "Reaction Devtools",
-    name: "reaction-devtools",
-    icon: "fa fa-code",
-    meta: {
-      version: "0.0.1"
-    },
-    autoEnable: true,
-    registry: [
-      {
-        name: "reaction-devtools-admin",
-        label: "Dev Tools",
-        provides: ["settings"],
-        container: "dashboard",
-        template: "DevTools",
-        icon: "fa fa-code"
-      }
-    ]
-  });
-}
+// Register package as ReactionCommerce package
+Reaction.registerPackage({
+  label: "Reaction Devtools",
+  name: "reaction-devtools",
+  icon: "fa fa-code",
+  meta: {
+    version: "0.0.1"
+  },
+  autoEnable: !!(process.env.NODE_ENV === "development"),
+  registry: [
+    {
+      name: "reaction-devtools-admin",
+      label: "Dev Tools",
+      provides: ["settings"],
+      container: "dashboard",
+      template: "DevTools",
+      icon: "fa fa-code"
+    }
+  ]
+});
