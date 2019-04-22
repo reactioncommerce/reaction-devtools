6d75c6e50e17627c6cb7ffe87b5098d7240254dc
Steven Rowney <steven.rowney@reaktor.com>
2019-04-22 11:09:38 +0400

    Remove wrap around of registerOperatorRoute in dev mode
    Signed-off-by: Steven Rowney <steven.rowney@reaktor.com>

--- client/index.js
+++ client/index.js
@@ -4,14 +4,12 @@ import React from "react";
 import { registerOperatorRoute } from "../../../../client/ui";
 import DevTools from "./components/devtools";
 
-if (process.env.NODE_ENV === "development") {
-  registerOperatorRoute({
-    isNavigationLink: true,
-    isSetting: true,
-    path: "/dev-tools",
-    mainComponent: DevTools,
-    // eslint-disable-next-line react/display-name, react/no-multi-comp
-    SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLaptop} {...props} />,
-    sidebarI18nLabel: "admin.settings.devToolsLabel"
-  });
-}
+registerOperatorRoute({
+  isNavigationLink: true,
+  isSetting: true,
+  path: "/dev-tools",
+  mainComponent: DevTools,
+  // eslint-disable-next-line react/display-name, react/no-multi-comp
+  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLaptop} {...props} />,
+  sidebarI18nLabel: "admin.settings.devToolsLabel"
+});
